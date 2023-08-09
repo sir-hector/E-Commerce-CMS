a13a543d41c924eb55bff779df9464260a262723
sir-hector <krauskarol7@gmail.com>
2023-08-09 21:53:35 +0200

    navbar + main nav component

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,6 +1,8 @@
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
+import Navbar from "@/components/navbar";
+
 import prismadb from "@/lib/prismadb";
 
 export default async function DashboardLaybout({
@@ -29,7 +31,7 @@ export default async function DashboardLaybout({
 
   return (
     <>
-      <div>This will be a Navbar</div>
+      <Navbar />
       {children}
     </>
   );
