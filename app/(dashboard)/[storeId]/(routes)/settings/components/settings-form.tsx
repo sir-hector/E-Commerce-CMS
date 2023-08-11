986ea56b21eda162e0c50da1a309446ff7917451
sir-hector <krauskarol7@gmail.com>
2023-08-11 17:01:55 +0200

    create settings form and heading

---
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Store } from "@prisma/client";
+import { Heading } from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Trash } from "lucide-react";
+
+interface SettingsFormProps {
+  initialData: Store;
+}
+
+export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading title="Settings" description="Manage stores settings" />
+        <Button variant="destructive" size="sm" onClick={() => {}}>
+          <Trash className="h-4 w-4" />
+        </Button>
+      </div>
+      
+    </>
+  );
+};
