8df9fba0848d50e93c2c05b0ededc5148a9059d3
sir-hector <krauskarol7@gmail.com>
2023-08-19 15:23:39 +0200

    add image uploader components

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboards-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboards-form.tsx
@@ -27,6 +27,7 @@ import { Input } from "@/components/ui/input";
 import { AlertModal } from "@/components/modals/alert-modal";
 import { ApiAlert } from "@/components/ui/api-alert";
 import { useOrigin } from "@/hooks/use-origin";
+import ImageUpload from "@/components/ui/image-uploader";
 
 const formSchema = z.object({
   label: z.string().min(1),
@@ -119,6 +120,24 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
           onSubmit={form.handleSubmit(onSubmit)}
           className="space-y-8 w-full"
         >
+          <FormField
+              control={form.control}
+              name="imageUrl"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Background Image</FormLabel>
+                  <FormControl>
+                    <ImageUpload 
+                    value={field.value ? [field.value]: []}
+                    disabled={loading}
+                    onChange={((url) => field.onChange(url))}
+                    onRemove={() => field.onChange("")}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
           <div className="grid grid-cols-3 gap-8">
             <FormField
               control={form.control}
