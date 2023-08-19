954941e755f72c3d95e7507250a1d80a33eac461
sir-hector <krauskarol7@gmail.com>
2023-08-19 14:29:02 +0200

    billboard form

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,18 +1,28 @@
 "use client";
 
+import { useParams } from "next/navigation";
+import { Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
+
 import { Heading } from "@/components/ui/heading";
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
 import { Separator } from "@radix-ui/react-separator";
 
+
 export const BillboardClient = () => {
+  const router = useRouter();
+  const params = useParams();
+
   return (
     <>
       <div className="flex items-center justify-between">
-        <Heading title="Billboards (0)" description="Menage billboards for your store"/>
-        <Button>
-            <Plus className="mr-2 h-4 w-4"/>
-            Add new
+        <Heading
+          title="Billboards (0)"
+          description="Menage billboards for your store"
+        />
+        <Button onClick={()=> router.push(`/${params.storeId}/billboards/new`)}>
+          <Plus className="mr-2 h-4 w-4" />
+          Add new
         </Button>
       </div>
       <Separator />
--- components/ui/heading.tsx
+++ components/ui/heading.tsx
@@ -9,7 +9,7 @@ export const Heading: React.FC<HeadingProps> = ({title, description}) => {
             <h2 className="text-3xl font-bold tracking-tight">
                 {title}
             </h2>
-            <p className="text-sm text-muted">
+            <p className="text-sm text-muted-foreground">
                 {description}
             </p>
         </div>
