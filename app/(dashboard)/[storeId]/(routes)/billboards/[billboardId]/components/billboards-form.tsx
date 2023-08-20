529ddca6a070fc14cf856772ce52814bffd60ae9
sir-hector <krauskarol7@gmail.com>
2023-08-20 11:53:03 +0200

    clean form

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboards-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboards-form.tsx
@@ -25,8 +25,6 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { AlertModal } from "@/components/modals/alert-modal";
-import { ApiAlert } from "@/components/ui/api-alert";
-import { useOrigin } from "@/hooks/use-origin";
 import ImageUpload from "@/components/ui/image-uploader";
 
 const formSchema = z.object({
@@ -46,7 +44,6 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
   const params = useParams();
   const router = useRouter();
 
-  const origin = useOrigin();
 
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
