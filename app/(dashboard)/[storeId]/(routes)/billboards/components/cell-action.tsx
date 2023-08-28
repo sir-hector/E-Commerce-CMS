9aa49c8a8401296e26c0dae298546fb5626cd05b
sir-hector <krauskarol7@gmail.com>
2023-08-28 19:54:28 +0200

    cell action interface

--- app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -1,3 +1,11 @@
+"use client"
+
+import { BillboardColumn } from "./columns"
+
+interface CellActionProps {
+    data: BillboardColumn
+}
+
 export const CellAction = () => {
     return (
         <div>Action</div>
