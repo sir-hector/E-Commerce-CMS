dbfffdf788e29e7fa0ace12a212c92875c948cf9
sir-hector <krauskarol7@gmail.com>
2023-08-30 20:21:02 +0200

    data table drop down menu

--- app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -1,13 +1,25 @@
-"use client"
+"use client";
 
-import { BillboardColumn } from "./columns"
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { BillboardColumn } from "./columns";
+import { Button } from "@/components/ui/button";
+import { MoreHorizontal } from "lucide-react";
 
 interface CellActionProps {
-    data: BillboardColumn
+  data: BillboardColumn;
 }
 
-export const CellAction: React.FC<CellActionProps> = ({data}) => {
-    return (
-        <div>Action</div>
-    )
-}
+export const CellAction: React.FC<CellActionProps> = ({ data }) => {
+  return(
+  <DropdownMenu>
+    <DropdownMenuTrigger asChild>
+      <Button variant="ghost" className="h-8 w-8 p-0">
+        <span className="sr-only">Open menu</span>
+        <MoreHorizontal className="h-4 w-4"/>
+      </Button>
+    </DropdownMenuTrigger>
+  </DropdownMenu>)
+};
