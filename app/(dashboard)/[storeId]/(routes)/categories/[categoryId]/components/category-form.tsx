1531ca3b118b91f9275cf072219217b524af9daa
sir-hector <krauskarol7@gmail.com>
2023-12-06 21:16:42 +0100

    fix router path in creating category form

--- app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/components/category-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/components/category-form.tsx
@@ -71,7 +71,7 @@ export const CategoryForm: React.FC<CategoryFormProps> = ({
         await axios.post(`/api/${params.storeId}/categories`, data);
       }
       router.refresh();
-      router.push(`${params.storeId}/categories`)
+      router.push(`/${params.storeId}/categories`)
       toast.success(toastMessage);
     } catch (error) {
       toast.error("Something went Wrong");
